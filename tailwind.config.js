af44e219f4f1782fea0323d9f61a036a61edc441
me <me@work.com>
2025-09-13 15:27:24 +0530

    Initial commit with Kalaverse frontend

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'fadeIn': 'fadeIn 0.5s ease-in-out forwards',
+      }
+    },
+  },
+  plugins: [],
